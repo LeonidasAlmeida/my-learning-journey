b34b0dce3ceb0a4d62b63803d471924cf29f9560
unknown <949leoni@gmail.com>
2025-12-29 12:12:43 -0300

    input of value in console

---
+++ python/09input_function.py
@@ -0,0 +1,8 @@
+#read data with input fuction
+num = int(input("Enter number :"));
+print(num)
+name1 = input("Enter name : ");
+print(name1)
+#printing type of input value
+print("type of number", type(num))
+print("type of name ",type(name1))
