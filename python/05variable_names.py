94e99f01147c56f087a1cc4c993b6241dbccd2c0
unknown <949leoni@gmail.com>
2025-12-29 07:37:45 -0300

    variable names in python, correct and incorrect way to create variables

---
+++ python/05variable_names.py
@@ -0,0 +1,20 @@
+#Variable names
+#Correct way to create variable
+counter = 100
+_count = 100
+name1 = "micheal"
+name2 = "mick"
+age = 28
+zara_salary = 10000
+print(counter)
+print(_count)
+print(name1)
+print(name2)
+print(zara_salary)
+
+# Incorrect way to create variables
+# 1counter = 100
+# $_соunt  = 100
+# mісhеаl-ѕаlаrу = 100000
+# рrіnt(1соuntеr)
+# рrіnt($count)
