985e759fcccf8aa38f31a1661fc92941531e7c34
unknown <949leoni@gmail.com>
2025-12-29 07:09:09 -0300

    print variables

---
+++ python/02printing_variables.py
@@ -0,0 +1,9 @@
+#print pyhton variables
+counter = 100 #creates a integer variable
+miles = 1000.0 #creates a floating point variable
+name = "micheal mick" #create a string varible
+
+print(counter)
+print(miles)
+print(name)
+
