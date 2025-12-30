7d056217680ae078957c84965e2cc95226fa82a2
unknown <949leoni@gmail.com>
2025-12-30 07:16:40 -0300

    List data type

---
+++ python/11list_data.py
@@ -0,0 +1,21 @@
+#list  data type
+
+#empty list
+List = []
+print(List)
+del List
+#list with value
+List = ['michealmick']
+print("\nList with the use of String")
+print(List)
+#creating a List with
+#the use multiple values
+List =["micheal","mick"]
+print("\nList containing multiple values: ")
+print(List[0])
+print(List[1])
+#creating a multi-dimensional List
+#(By nesting a list inside a list)
+List = [['micheal'],['Geeks']]
+print("\nMulti - Dimensional List")
+print(List)
