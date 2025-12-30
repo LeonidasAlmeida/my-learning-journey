13bc823e1bc0e41213b40e0e51875911f1f0c770
unknown <949leoni@gmail.com>
2025-12-30 13:08:26 -0300

    array data type

--- python/11list_data.py
+++ python/11list_data.py
@@ -1,5 +1,7 @@
 #list  data type
-
+## append()add ond e end of list
+## insert(index,value) add in expecific place
+## extend(list) add a several values in a list
 #empty list
 List = []
 print(List)
@@ -18,4 +20,23 @@ print(List[1])
 #(By nesting a list inside a list)
 List = [['micheal'],['Geeks']]
 print("\nMulti - Dimensional List")
+print(List)
+#adding element of list
+List =[1,2,3]
+print(List)
+print(len(List))
+List2 = [4,5]   
+x =6
+List.append(x)
+print(List)
+for i in List2:
+    List.insert(len(List)-1,i)
+print(List)
+List.extend(List2)
+print(List)
+List.remove(4)
+print(List)
+List.pop()
+print(List)
+List.pop(1)
 print(List)
