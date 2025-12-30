39c088db297c8b3a9747c88025596b6cd26162d8
unknown <949leoni@gmail.com>
2025-12-30 07:39:02 -0300

    set data type

---
+++ python/12set_data.py
@@ -0,0 +1,9 @@
+#create set data type
+set1 = set()
+print(set1)
+#creating with use of String
+set1 = set("michaelmick")
+print(set1)
+#creating with list
+set1=set(["michael","For","michael"])
+print(set1)
