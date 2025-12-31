b6497b360ca9cc585592f32741dd96811bbd72f2
unknown <949leoni@gmail.com>
2025-12-31 07:37:42 -0300

    Creting matrix

---
+++ deep-leaning/02creating_matrix.py
@@ -0,0 +1,9 @@
+#creating a matrix 
+#creating a array with two dimension
+#import libray
+import numpy as np
+#create a matrix
+matrix = np.array([[1, 2],
+                   [1, 2],
+                   [1, 2]])
+print(matrix)
