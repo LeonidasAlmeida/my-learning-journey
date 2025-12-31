7e9377970b8fc75445fdfc1a45e0cbe7676361dd
unknown <949leoni@gmail.com>
2025-12-31 10:52:21 -0300

    lambda function

---
+++ deep-leaning/08lambda_functions.py
@@ -0,0 +1,10 @@
+#creating lambda functions
+#normal function
+def soma(x,y):
+    return x + y
+#view output
+print(soma(10,19))
+#lambda function
+soma2 = lambda x , y : x + y
+#view output
+print(soma2(10,19))
